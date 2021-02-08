3c842b5ea58215f3dbbcd2a40d16d871c5545b39
rohith <rrohith2001@gmail.com>
2021-02-08 14:52:12 +0530

    HoF
    Partially_Applied_Funtions - Currying

--- .worksheet/src/HigherOrderFunctions.scala
+++ .worksheet/src/HigherOrderFunctions.scala
@@ -1,90 +1,111 @@
 object HigherOrderFunctions {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(57); 
-  println("Welcome to HoF");$skip(138); 
-  
-  def sumInts(x:Int, y:Int) = {
-  
-  	def iter(x:Int,acc:Int):Int={
-  		if(x>y)acc;
-  		else iter(x+1,acc+x)
-  	}
-  	
-  	iter(x,0)
-  };System.out.println("""sumInts: (x: Int, y: Int)Int""");$skip(19); val res$0 = 
-  
-  sumInts(1,10);System.out.println("""res0: Int = """ + $show(res$0));$skip(142); 
-  
-  def sumSqInts(x:Int, y:Int) = {
-  
-  	def iter(x:Int,acc:Int):Int={
-  		if(x>y)acc;
-  		else iter(x+1,acc+x*x)
-  	}
-  	
-  	iter(x,0)
-  };System.out.println("""sumSqInts: (x: Int, y: Int)Int""");$skip(21); val res$1 = 
-  
-  sumSqInts(1,10);System.out.println("""res1: Int = """ + $show(res$1));$skip(220); 
-  
+  println("Welcome to HoF");$skip(156); 
+
+  def sumInts(x: Int, y: Int) = {
+
+    def iter(x: Int, acc: Int): Int = {
+      if (x > y) acc;
+      else iter(x + 1, acc + x)
+    }
+
+    iter(x, 0)
+  };System.out.println("""sumInts: (x: Int, y: Int)Int""");$skip(18); val res$0 = 
+
+  sumInts(1, 10);System.out.println("""res0: Int = """ + $show(res$0));$skip(162); 
+
+  def sumSqInts(x: Int, y: Int) = {
+
+    def iter(x: Int, acc: Int): Int = {
+      if (x > y) acc;
+      else iter(x + 1, acc + x * x)
+    }
+
+    iter(x, 0)
+  };System.out.println("""sumSqInts: (x: Int, y: Int)Int""");$skip(20); val res$1 = 
+
+  sumSqInts(1, 10);System.out.println("""res1: Int = """ + $show(res$1));$skip(241); 
+
   /*
   	HigherOrderFunction :
   	Generalises the series summation
   */
-   def sum(f:Int=>Int,x:Int, y:Int) = {
-  
-  	def iter(x:Int,acc:Int):Int={
-  		if(x>y)acc;
-  		else iter(x+1,acc+f(x))
-  	}
-  	
-  	iter(x,0)
-  };System.out.println("""sum: (f: Int => Int, x: Int, y: Int)Int""");$skip(20); val res$2 = 
-  
-  sum(x=>x,1,10);System.out.println("""res2: Int = """ + $show(res$2));$skip(19); val res$3 = 
-  sum(x=>x*x,1,10);System.out.println("""res3: Int = """ + $show(res$3));$skip(21); val res$4 = 
-  sum(x=>x*x*x,1,10);System.out.println("""res4: Int = """ + $show(res$4));$skip(310); 
-  
+  def sum(f: Int => Int, x: Int, y: Int) = {
+
+    def iter(x: Int, acc: Int): Int = {
+      if (x > y) acc;
+      else iter(x + 1, acc + f(x))
+    }
+
+    iter(x, 0)
+  };System.out.println("""sum: (f: Int => Int, x: Int, y: Int)Int""");$skip(22); val res$2 = 
+
+  sum(x => x, 1, 10);System.out.println("""res2: Int = """ + $show(res$2));$skip(25); val res$3 = 
+  sum(x => x * x, 1, 10);System.out.println("""res3: Int = """ + $show(res$3));$skip(29); val res$4 = 
+  sum(x => x * x * x, 1, 10);System.out.println("""res4: Int = """ + $show(res$4));$skip(319); 
+
   //We want to generalise all series operations
-  def mapreduce(x:Int,y:Int,
-  					map : Int=>Int = x=>x,
-  					reduce:(Int,Int)=>Int = (a,b)=>a+b,
-  					iden: Int = 0
-  					) = {
-  					
-  	def iter(x:Int,acc:Int):Int={
-  		if(x>y)acc;
-  		else iter(x+1,reduce(acc,map(x)))
-  	}
-  	
-  	iter(x,iden)
- };System.out.println("""mapreduce: (x: Int, y: Int, map: Int => Int, reduce: (Int, Int) => Int, iden: Int)Int""");$skip(281); 
- 
-   def mapreduce1
-  					(
-  					map : Int=>Int = x=>x,
-  					reduce:(Int,Int)=>Int = (a,b)=>a+b,
-  					iden: Int = 0
-  					)
-  					(x:Int)(y:Int) = {
-  					
-  	def iter(x:Int,acc:Int):Int={
-  		if(x>y)acc;
-  		else iter(x+1,reduce(acc,map(x)))
-  	}
-  	
-  	iter(x,iden)
- };System.out.println("""mapreduce1: (map: Int => Int, reduce: (Int, Int) => Int, iden: Int)(x: Int)(y: Int)Int""");$skip(44); val res$5 = 
- 
- 		//HoF for summation
-  	mapreduce(1,10);System.out.println("""res5: Int = """ + $show(res$5));$skip(26); val res$6 = 
-  	mapreduce(1,10,x=>x*x);System.out.println("""res6: Int = """ + $show(res$6));$skip(66); val res$7 = 
-  	
-  	//factorial example
-  	mapreduce1(x=>x,(u,v)=>u*v,1)(1)(5);System.out.println("""res7: Int = """ + $show(res$7));$skip(72); 
-  	
-  	//Partially Applied
-  	val prod = mapreduce1(x=>x,(u,v)=>u*v,1)_;System.out.println("""prod  : Int => (Int => Int) = """ + $show(prod ));$skip(14); val res$8 = 
-  	prod(1)(5);System.out.println("""res8: Int = """ + $show(res$8));$skip(107); 
-  	
-  	//factorial using partially applied general function
-  	val fct = mapreduce1(x=>x,(u,v)=>u*v,1)(1)_;System.out.println("""fct  : Int => Int = """ + $show(fct ))}
+  def mapreduce(x: Int, y: Int,
+    map: Int => Int = x => x,
+    reduce: (Int, Int) => Int = (a, b) => a + b,
+    iden: Int = 0) = {
+
+    def iter(x: Int, acc: Int): Int = {
+      if (x > y) acc;
+      else iter(x + 1, reduce(acc, map(x)))
+    }
+
+    iter(x, iden)
+  };System.out.println("""mapreduce: (x: Int, y: Int, map: Int => Int, reduce: (Int, Int) => Int, iden: Int)Int""");$skip(313); 
+
+  def mapreduce1(
+    map: Int => Int = x => x,
+    reduce: (Int, Int) => Int = (a, b) => a + b,
+    iden: Int = 0)
+    (x: Int)(y: Int) = { //The Parameters have been curried
+
+    def iter(x: Int, acc: Int): Int = {
+      if (x > y) acc;
+      else iter(x + 1, reduce(acc, map(x)))
+    }
+
+    iter(x, iden)
+  };System.out.println("""mapreduce1: (map: Int => Int, reduce: (Int, Int) => Int, iden: Int)(x: Int)(y: Int)Int""");$skip(42); val res$5 = 
+
+  //HoF for summation
+  mapreduce(1, 10);System.out.println("""res5: Int = """ + $show(res$5));$skip(31); val res$6 = 
+  mapreduce(1, 10, x => x * x);System.out.println("""res6: Int = """ + $show(res$6));$skip(70); val res$7 = 
+
+  //factorial example
+  mapreduce1(x => x, (u, v) => u * v, 1)(1)(5);System.out.println("""res7: Int = """ + $show(res$7));$skip(76); 
+
+  //Partially Applied
+  val prod = mapreduce1(x => x, (u, v) => u * v, 1)_;System.out.println("""prod  : Int => (Int => Int) = """ + $show(prod ));$skip(13); val res$8 = 
+  prod(1)(5);System.out.println("""res8: Int = """ + $show(res$8));$skip(111); 
+
+  //factorial using partially applied general function
+  val fct = mapreduce1(x => x, (u, v) => u * v, 1)(1)_;System.out.println("""fct  : Int => Int = """ + $show(fct ));$skip(9); val res$9 = 
+  fct(6);System.out.println("""res9: Int = """ + $show(res$9));$skip(337); 
+
+  def mapreduce2(
+    map: Int => Int = x => x,
+    reduce: (Int, Int) => Int = (a, b) => a + b,
+    iden: Int = 0): (Int, Int) => Int = {
+    def fn(x: Int, y: Int) = {
+
+      def iter(x: Int, acc: Int): Int = {
+        if (x > y) acc;
+        else iter(x + 1, reduce(acc, map(x)))
+      }
+
+      iter(x, iden)
+    }
+    return fn
+  };System.out.println("""mapreduce2: (map: Int => Int, reduce: (Int, Int) => Int, iden: Int)(Int, Int) => Int""");$skip(46); 
+  val sumints = mapreduce2(x=>x,(u,v)=>u+v,0);System.out.println("""sumints  : (Int, Int) => Int = """ + $show(sumints ));$skip(16); val res$10 = 
+  sumints(1,10);System.out.println("""res10: Int = """ + $show(res$10));$skip(53); 
+  
+  val sumSqints = mapreduce2(x=>x*x,(u,v)=>u+v,0);System.out.println("""sumSqints  : (Int, Int) => Int = """ + $show(sumSqints ));$skip(18); val res$11 = 
+  sumSqints(1,10);System.out.println("""res11: Int = """ + $show(res$11))}
+  
+
 }
--- HigherOrderFunctions.sc
+++ HigherOrderFunctions.sc
@@ -1,94 +1,119 @@
 object HigherOrderFunctions {
   println("Welcome to HoF")                       //> Welcome to HoF
-  
-  def sumInts(x:Int, y:Int) = {
-  
-  	def iter(x:Int,acc:Int):Int={
-  		if(x>y)acc;
-  		else iter(x+1,acc+x)
-  	}
-  	
-  	iter(x,0)
+
+  def sumInts(x: Int, y: Int) = {
+
+    def iter(x: Int, acc: Int): Int = {
+      if (x > y) acc;
+      else iter(x + 1, acc + x)
+    }
+
+    iter(x, 0)
   }                                               //> sumInts: (x: Int, y: Int)Int
-  
-  sumInts(1,10)                                   //> res0: Int = 55
-  
-  def sumSqInts(x:Int, y:Int) = {
-  
-  	def iter(x:Int,acc:Int):Int={
-  		if(x>y)acc;
-  		else iter(x+1,acc+x*x)
-  	}
-  	
-  	iter(x,0)
+
+  sumInts(1, 10)                                  //> res0: Int = 55
+
+  def sumSqInts(x: Int, y: Int) = {
+
+    def iter(x: Int, acc: Int): Int = {
+      if (x > y) acc;
+      else iter(x + 1, acc + x * x)
+    }
+
+    iter(x, 0)
   }                                               //> sumSqInts: (x: Int, y: Int)Int
-  
-  sumSqInts(1,10)                                 //> res1: Int = 385
-  
+
+  sumSqInts(1, 10)                                //> res1: Int = 385
+
   /*
   	HigherOrderFunction :
   	Generalises the series summation
   */
-   def sum(f:Int=>Int,x:Int, y:Int) = {
-  
-  	def iter(x:Int,acc:Int):Int={
-  		if(x>y)acc;
-  		else iter(x+1,acc+f(x))
-  	}
-  	
-  	iter(x,0)
+  def sum(f: Int => Int, x: Int, y: Int) = {
+
+    def iter(x: Int, acc: Int): Int = {
+      if (x > y) acc;
+      else iter(x + 1, acc + f(x))
+    }
+
+    iter(x, 0)
   }                                               //> sum: (f: Int => Int, x: Int, y: Int)Int
-  
-  sum(x=>x,1,10)                                  //> res2: Int = 55
-  sum(x=>x*x,1,10)                                //> res3: Int = 385
-  sum(x=>x*x*x,1,10)                              //> res4: Int = 3025
-  
+
+  sum(x => x, 1, 10)                              //> res2: Int = 55
+  sum(x => x * x, 1, 10)                          //> res3: Int = 385
+  sum(x => x * x * x, 1, 10)                      //> res4: Int = 3025
+
   //We want to generalise all series operations
-  def mapreduce(x:Int,y:Int,
-  					map : Int=>Int = x=>x,
-  					reduce:(Int,Int)=>Int = (a,b)=>a+b,
-  					iden: Int = 0
-  					) = {
-  					
-  	def iter(x:Int,acc:Int):Int={
-  		if(x>y)acc;
-  		else iter(x+1,reduce(acc,map(x)))
-  	}
-  	
-  	iter(x,iden)
- }                                                //> mapreduce: (x: Int, y: Int, map: Int => Int, reduce: (Int, Int) => Int, iden
-                                                  //| : Int)Int
- 
-   def mapreduce1
-  					(
-  					map : Int=>Int = x=>x,
-  					reduce:(Int,Int)=>Int = (a,b)=>a+b,
-  					iden: Int = 0
-  					)
-  					(x:Int)(y:Int) = {
-  					
-  	def iter(x:Int,acc:Int):Int={
-  		if(x>y)acc;
-  		else iter(x+1,reduce(acc,map(x)))
-  	}
-  	
-  	iter(x,iden)
- }                                                //> mapreduce1: (map: Int => Int, reduce: (Int, Int) => Int, iden: Int)(x: Int)
+  def mapreduce(x: Int, y: Int,
+    map: Int => Int = x => x,
+    reduce: (Int, Int) => Int = (a, b) => a + b,
+    iden: Int = 0) = {
+
+    def iter(x: Int, acc: Int): Int = {
+      if (x > y) acc;
+      else iter(x + 1, reduce(acc, map(x)))
+    }
+
+    iter(x, iden)
+  }                                               //> mapreduce: (x: Int, y: Int, map: Int => Int, reduce: (Int, Int) => Int, ide
+                                                  //| n: Int)Int
+
+  def mapreduce1(
+    map: Int => Int = x => x,
+    reduce: (Int, Int) => Int = (a, b) => a + b,
+    iden: Int = 0)
+    (x: Int)(y: Int) = { //The Parameters have been curried
+
+    def iter(x: Int, acc: Int): Int = {
+      if (x > y) acc;
+      else iter(x + 1, reduce(acc, map(x)))
+    }
+
+    iter(x, iden)
+  }                                               //> mapreduce1: (map: Int => Int, reduce: (Int, Int) => Int, iden: Int)(x: Int)
                                                   //| (y: Int)Int
- 
- 		//HoF for summation
-  	mapreduce(1,10)                           //> res5: Int = 55
-  	mapreduce(1,10,x=>x*x)                    //> res6: Int = 385
-  	
-  	//factorial example
-  	mapreduce1(x=>x,(u,v)=>u*v,1)(1)(5)       //> res7: Int = 120
-  	
-  	//Partially Applied
-  	val prod = mapreduce1(x=>x,(u,v)=>u*v,1)_ //> prod  : Int => (Int => Int) = HigherOrderFunctions$$$Lambda$18/997110508@6e
+
+  //HoF for summation
+  mapreduce(1, 10)                                //> res5: Int = 55
+  mapreduce(1, 10, x => x * x)                    //> res6: Int = 385
+
+  //factorial example
+  mapreduce1(x => x, (u, v) => u * v, 1)(1)(5)    //> res7: Int = 120
+
+  //Partially Applied
+  val prod = mapreduce1(x => x, (u, v) => u * v, 1)_
+                                                  //> prod  : Int => (Int => Int) = HigherOrderFunctions$$$Lambda$18/997110508@6e
                                                   //| 8dacdf
-  	prod(1)(5)                                //> res8: Int = 120
-  	
-  	//factorial using partially applied general function
-  	val fct = mapreduce1(x=>x,(u,v)=>u*v,1)(1)_
+  prod(1)(5)                                      //> res8: Int = 120
+
+  //factorial using partially applied general function
+  val fct = mapreduce1(x => x, (u, v) => u * v, 1)(1)_
                                                   //> fct  : Int => Int = HigherOrderFunctions$$$Lambda$22/398887205@7e0ea639
+  fct(6)                                          //> res9: Int = 720
+
+  def mapreduce2(
+    map: Int => Int = x => x,
+    reduce: (Int, Int) => Int = (a, b) => a + b,
+    iden: Int = 0): (Int, Int) => Int = {
+    def fn(x: Int, y: Int) = {
+
+      def iter(x: Int, acc: Int): Int = {
+        if (x > y) acc;
+        else iter(x + 1, reduce(acc, map(x)))
+      }
+
+      iter(x, iden)
+    }
+    return fn
+  }                                               //> mapreduce2: (map: Int => Int, reduce: (Int, Int) => Int, iden: Int)(Int, In
+                                                  //| t) => Int
+  val sumints = mapreduce2(x=>x,(u,v)=>u+v,0)     //> sumints  : (Int, Int) => Int = HigherOrderFunctions$$$Lambda$25/1908316405@
+                                                  //| 6fadae5d
+  sumints(1,10)                                   //> res10: Int = 55
+  
+  val sumSqints = mapreduce2(x=>x*x,(u,v)=>u+v,0) //> sumSqints  : (Int, Int) => Int = HigherOrderFunctions$$$Lambda$25/190831640
+                                                  //| 5@2812cbfa
+  sumSqints(1,10)                                 //> res11: Int = 385
+  
+
 }
